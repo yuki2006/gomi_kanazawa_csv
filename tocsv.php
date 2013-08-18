73348426165e73e7fcea96d89d8bc3979ee17b70
yuki2006 <yagfair@gmail.com>
2013-08-19 00:35:19 +0900

    微調整

--- tocsv.php
+++ tocsv.php
@@ -1,6 +1,9 @@
 <?php
 	$file=file_get_contents("source.dat");
 	$mdfile=fopen("README.md","w+");
+	fwrite($mdfile, "金沢市のゴミの予定をGoogleカレンダーへ！".PHP_EOL);
+	fwrite($mdfile, "ご利用の地域のCSVをGoogleカレンダーにインポートしてください".PHP_EOL);
+
 	$row=explode(PHP_EOL, $file);
 	foreach ($row as $key => $value) {
 		$cell=explode(",", $value);
@@ -9,7 +12,6 @@
 				fclose($fp);				
 			}
 			$fp=fopen("csv/".$value.".csv", "w+");
-			fwrite($mdfile, "金沢市のゴミの予定をGoogleカレンダーへ！".PHP_EOL);
 			fwrite($mdfile, "-$value地区".PHP_EOL);
 			fwrite($mdfile, "[$value](https://raw.github.com/yuki2006/gomi_kanazawa_csv/master/csv/$value.csv)");
 			fwrite($mdfile, PHP_EOL.PHP_EOL);
