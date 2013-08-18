60e76ac51df74cd9da5e614caabbe3d648aeaab8
yuki2006 <yagfair@gmail.com>
2013-08-19 00:54:19 +0900

    icalのテスト

--- tocsv.php
+++ tocsv.php
@@ -11,13 +11,36 @@
 			if ($fp!==null){
 				fclose($fp);				
 			}
-			$fp=fopen("csv/".$value.".csv", "w+");
+			$fp=fopen("ical/".$value.".ical", "w+");
+$ical=<<<eot
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//ono yuuki/Code For Kanazawa//EN
+CALSCALE:GREGORIAN
+X-WR-CALNAME:$value地区のゴミの日
+eot;
+fwrite($fp, $ical);
+
 			fwrite($mdfile, "-$value地区".PHP_EOL);
 			fwrite($mdfile, "[$value](https://raw.github.com/yuki2006/gomi_kanazawa_csv/master/csv/$value.csv)");
 			fwrite($mdfile, PHP_EOL.PHP_EOL);
 		}else{
-			fwrite($fp, $value);
-			fwrite($fp, PHP_EOL);
+			if ($cell[0]=="Subject"){
+				continue;
+			}
+
+			$tmp=explode("/", $cell[1]);
+$date=sprintf("%04d",$tmp[0]).	sprintf("%02d",$tmp[1]).sprintf("%02d",$tmp[2]);	
+$daylabel=$cell[0]."ゴミの日";
+$ical=<<<eot
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:$date
+SUMMARY:$daylabel
+DTEND;VALUE=DATE:$date
+END:VEVENT
+eot;
+
+			fwrite($fp, $ical);
 		}
 	}
 	fclose($mdfile);
