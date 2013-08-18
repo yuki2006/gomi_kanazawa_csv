aaaf1b30b63eda51a5afbf2a0b794f350ddc91f9
yuki2006 <yagfair@gmail.com>
2013-08-19 00:29:36 +0900

    ReadMeを整備

--- tocsv.php
+++ tocsv.php
@@ -1,5 +1,6 @@
 <?php
 	$file=file_get_contents("source.dat");
+	$mdfile=fopen("README.md","w+");
 	$row=explode(PHP_EOL, $file);
 	foreach ($row as $key => $value) {
 		$cell=explode(",", $value);
@@ -7,11 +8,15 @@
 			if ($fp!==null){
 				fclose($fp);				
 			}
-			$fp=fopen($value.".csv", "w+");
+			$fp=fopen("csv/".$value.".csv", "w+");
+			fwrite($mdfile, "-$value".PHP_EOL);
+			fwrite($mdfile, "[$value](https://github.com/yuki2006/gomi_kanazawa_csv/blob/master/%E9%9E%8D%E6%9C%88.csv)");
+			fwrite($mdfile, PHP_EOL);
 		}else{
 			fwrite($fp, $value);
 			fwrite($fp, PHP_EOL);
 		}
 	}
+	fclose($mdfile);
 
 ?>
